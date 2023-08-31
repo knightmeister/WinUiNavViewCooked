ac0b569b8d591dbf776d561f12491c665b47a8d9
knightmeister <sambknight@gmail.com>
2023-09-01 07:45:22 +1000

    Add comment

--- App1/MainWindow.xaml.cs
+++ App1/MainWindow.xaml.cs
@@ -37,7 +37,8 @@ namespace App1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            // If the SelectedItem in ctor is commented out it won't crash, and clicking it in the button allows assignment
+            NavView.SelectedItem = nvi1;
         }
     }
 }
